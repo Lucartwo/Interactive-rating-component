474ceeabf6d77d40cdcfd7d34d3698f9a7f5ecd6
Lucas Bomfim <lucasbomfim1@gmail.com>
2022-05-25 20:57:55 +0000

    alterações feitas no JS e no CSS para transição de submit

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-const buttons = document.querySelectorAll("button.rating")
+const buttons = document.querySelectorAll("button.rating");
+const sub = document.querySelector("#sub");
+const enterjs = document.querySelector(".enterjs");
+const thankscard = document.querySelector("#thankscard")
+const principalcard = document.querySelector("#principalcard")
 
 let ratingVote;
 
 function buttonText(event) {
     ratingVote = event.currentTarget.innerHTML;
-    buttons.forEach(button=>{
+    buttons.forEach(button => {
         button.classList.remove("selected")
     })
     event.currentTarget.classList.toggle("selected")
     console.log(ratingVote);
 }
 
-buttons.forEach(button =>{
-   button.addEventListener("click",buttonText)
+buttons.forEach(button => {
+    button.addEventListener("click", buttonText)
+})
+
+function Send(sent) {
+    enterjs.innerHTML = `You selected ${sent} out of 5`;
+    principalcard.innerHTML = thankscard.innerHTML;
+}
+sub.addEventListener("click", () => {
+    Send(ratingVote)
 })
 
 
